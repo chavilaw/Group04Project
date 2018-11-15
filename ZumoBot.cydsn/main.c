c601019195d11f63e8474603c4cf907190ccbdd9
Eliza Brinkmann <eliza.brinkmann@metropolia.fi>
2018-11-15 12:45:10 +0200

    Ha

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -945,26 +945,29 @@ reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center s
 
 for(;;)
     {
-        // read raw sensor values
-        reflectance_read(&ref);
-        // print out each period of reflectance sensors
-        printf("REF: l3:%d. l2:%d. l1:%d. r1:%d. r2:%d. r3:%d.\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       
-        
         // read digital values that are based on threshold. 0 = white, 1 = black
-        // when blackness value is over threshold the sensors reads 1, otherwise 0
         reflectance_digital(&dig); 
-        //print out 0 or 1 according to results of reflectance period
-        printf("DIG l3:%d. l2:%d. l1:%d. r1:%d. r2:%d. r3:%d.\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);       
-        
-        vTaskDelay(200);
+        printf("DIG l3:%d. l2:%d. l1:%d. r1:%d. r2:%d. r3:%d.\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);         
+        vTaskDelay(0);
     }
-//reflectance_digital(&dig);// reading the digital response from reflecctors in dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3
-//printf("l3:%d. l2:%d. l1:%d. r1:%d. r2:%d. r3:%d.\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);
+    if (SW1_Read()== 0)// button press
+    	Go_Stop();
+
+
 
 }
 
 #endif 
+void Go_Stop (void)
+{
+	motor_start();
+	motor_forward(0,0);
 
+if (dig.l3 && dig.r3 == 1)
+	motor_forward(0,0);
+else 
+	motor_forward(150,1);
+}
 
  
 /* [] END OF FILE */
