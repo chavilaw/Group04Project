143568659b911b895a9b9645c05d9f2fceac9763
melindav <melindav@metropolia.fi>
2018-11-29 16:14:12 +0200

    final

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -1349,7 +1349,7 @@ void random_reverse()
 #endif
 
 
-#if 1
+#if 0
 // Assignment 1 week 5 
 void zmain(void)
 {  
@@ -1506,9 +1506,140 @@ reflectance_digital(&dig);
     motor_forward(100,0);
     vTaskDelay(10);
 }
+#endif
+
+#if 1
+    //follow the line
+void Go_Stop (void);
+void Go_Stop2 (void);
+struct sensors_ dig;
+void Follow_Line_Stop(void);
+void tankturn_right();
+void tankturn_left();
 
 
+
+void zmain(void)
+{
+    reflectance_start();
+    reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
+
+// void IR_wait(void); //wait for any IR 
+
+  for(;;)
+    {
+        // read digital values that are based on threshold. 0 = white, 1 = black
+        reflectance_digital(&dig); 
+        printf("DIG l3:%d. l2:%d. l1:%d. r1:%d. r2:%d. r3:%d.\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);         
+        vTaskDelay(0);
+      if (SW1_Read()== 0)// button press
+       {
+         Go_Stop();
+         IR_Start(); // start IR receiving
+         IR_flush(); // clear IR receive buffer
+   		 IR_wait();
+   	     Follow_Line_Stop();
+       }
+      else
+        motor_forward(0,0);
+
+    }
 }
+
+void Go_Stop (void)
+{
+   
+	motor_start();
+	motor_forward(0,0);
+    
+  while (1)
+  {
+     reflectance_digital(&dig);
+     while ((dig.l3 == 0) && (dig.r3 == 0))
+     {
+       motor_forward(100,0);
+       reflectance_digital(&dig);
+     }
+    if ((dig.l3 == 1) && (dig.r3 == 1))
+    {
+        motor_forward(0,0);
+        reflectance_digital(&dig);
+        break;
+    }
+  }
+}
+
+void Go_Stop2 (void)
+{
+	uint8_t i=0;
+    uint8_t enable=0;
+	
+	while(i<3)
+	{
+        
+        reflectance_digital(&dig);
+		if (((dig.l3 == 1) && (dig.r3 == 1))&& enable==1)
+        {
+         i++;
+            enable=0;
+           
+        }
+		else if(!((dig.l3 == 1) && (dig.r3 == 1))&& enable ==0) 
+		{	
+        enable=1;
+        }
+        else
+        motor_forward(75,1);
+    }   
+    motor_forward(0,0);
+}
+void Follow_Line_Stop(void)
+{
+    uint8_t enable=0;
+    int i=0;
+	while (1)
+	{
+		reflectance_digital(&dig);
+		if ((dig.l1 == 1) && (dig.r1 == 1))
+		{
+			motor_forward(150,0);
+			reflectance_digital(&dig);
+		}
+		else if ((dig.l1 == 0) && (dig.r1 == 1))
+		{
+		tankturn_right(10,50,0);
+		reflectance_digital(&dig);
+		}
+		else if ((dig.l1 == 1) && (dig.r1 == 0))
+		{
+		tankturn_left(50,10,0);
+		reflectance_digital(&dig);
+		}
+		else if ((dig.l3 == 1) && (dig.r3 == 1)) 
+        {
+            motor_forward(0,0);
+            reflectance_digital(&dig);
+        }
+	}
+   
+}void tankturn_right(f_speed, b_speed, delay)
+{
+	MotorDirLeft_Write(0);      // set LeftMotor forward mode
+    MotorDirRight_Write(1);     // set RightMotor backward mode
+    PWM_WriteCompare1(f_speed); 
+    PWM_WriteCompare2(b_speed); 
+    vTaskDelay(delay);
+}
+void tankturn_left(f_speed, b_speed, delay)
+{
+	MotorDirLeft_Write(1);      // set LeftMotor backward mode
+    MotorDirRight_Write(0);     // set RightMotor forward mode
+    PWM_WriteCompare1(f_speed); 
+    PWM_WriteCompare2(b_speed); 
+    vTaskDelay(delay);
+}
+
+
 #endif
 
 /* [] END OF FILE */
